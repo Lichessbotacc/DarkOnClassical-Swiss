4810c0a15050c5a06883eda87fb3db1f54605fd9
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-04-08 13:30:59 +0200

    Update TEAM_ID and tournament options

---
+++ Swiss.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import requests
+import os
+import sys
+import random
+from datetime import datetime, timedelta
+import pytz
+import time  # Import time for sleep function
+
+TEAM_ID = "darkonclassical"
+
+# Token aus ENV
+API_TOKEN = os.getenv("KEY")
+if not API_TOKEN:
+    sys.exit("Error: API token not found. Please set KEY environment variable.")
+
+# Turnieroptionen mit klaren Namen
+OPTIONS = [
+
+    {"name": "DarkOnClassical 20+10",                "clock": {"limit": 1200, "increment": 10},  "nbRounds": 5},
+    {"name": "DarkOnClassical 30+0",                "clock": {"limit": 1800, "increment": 0},  "nbRounds": 5}, #30+0
+    {"name": "DarkOnClassical 30+20",                "clock": {"limit": 1800, "increment": 20},  "nbRounds": 5}, #30+20
+]
+
+def utc_millis_for_hour(hour):
+    utc = pytz.utc
+    now = datetime.now(utc)
+    tomorrow = now + timedelta(days=1)
+    start = datetime(tomorrow.year, tomorrow.month, tomorrow.day, hour, 45, tzinfo=utc)
+    return int(start.timestamp() * 1000), start
+
+def read_description():
+    path = os.path.join(os.path.dirname(__file__), "description.txt")
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            return f.read().strip()
+    return "Welcome to our Swiss tournament!"
+
+def create_swiss():
+    for hour in range(24):
+        option = random.choice(OPTIONS)
+        startDate, start_dt = utc_millis_for_hour(hour)
+        payload = {
+            "name": f"{option['name']} ",
+            "clock.limit": option["clock"]["limit"],
+            "clock.increment": option["clock"]["increment"],
+            "nbRounds": option["nbRounds"],
+            "rated": "true",
+            "description": read_description(),
+            "startsAt": startDate,
+            "conditions.playYourGames": "true"
+        }
+        url = f"https://lichess.org/api/swiss/new/{TEAM_ID}"
+        headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+        print(f"➡ Creating: {payload['name']} "
+              f"({payload['clock.limit']//60}+{payload['clock.increment']}, "
+              f"{payload['nbRounds']}R, Start {start_dt} UTC)")
+
+        r = requests.post(url, data=payload, headers=headers)
+
+        if r.status_code == 200:
+            data = r.json()
+            print("✅ Tournament created!")
+            print("URL:", f"https://lichess.org/swiss/{data.get('id')}")
+        else:
+            print("❌ Error:", r.status_code, r.text)
+
+        time.sleep(1)  # Wait 2 seconds between requests to avoid simultaneous creation
+
+if __name__ == "__main__":
+    create_swiss()
